789c11680878c3096066a138a85215b012f094e2
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-12 08:41:35 +0100

    updated

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- A script that creates a users table
 -- Create table syntax with constraints
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id int NOT NULL AUTO_INCREMENT,
   email varchar(255) NOT NULL,
   name varchar(255),
