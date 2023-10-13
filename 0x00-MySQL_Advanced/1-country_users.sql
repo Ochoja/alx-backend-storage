44510b4839fdc6661361b3c0235d60087f96faf1
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-13 03:58:33 +0100

    updated

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
     name varchar(255),
-    country enum('US', 'CO', 'TN'),
+    country enum('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT user_email UNIQUE(email)
 );
