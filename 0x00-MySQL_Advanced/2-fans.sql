0e0d59eca9d9a35e224a7346e9c446b2180ebc6b
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-13 04:56:28 +0100

    added task 2 file

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- Ranks country origin and bands from metal_band.sql
+-- Select distict countries, sums the fans and orders them
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands GROUP BY origin 
+ORDER BY nb_fans DESC;
