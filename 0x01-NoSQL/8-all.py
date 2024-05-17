6e72c4e73b1ecc8902a80714b67599281cf9f435
Daniel Ochoja <danielochoja@yahoo.com>
2024-05-17 13:35:04 +0100

    updated 8-all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 """List all documents in collection"""
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """List all documents in mongo_collection"""
-    client = MongoClient()
     docs = []
 
-    for doc in mongo_collection:
-        docs.append(doc)
-
-    if docs == []:
+    try:
+        for doc in mongo_collection.find():
+            docs.append(doc)
+        return docs
+    except Exception:
         return None
-
-    return docs
